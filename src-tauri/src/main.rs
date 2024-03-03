f1c784fe1b010fbda5a50ea2c11e28e8ed139564
TejasBhovad <tejasbhovad@gmail.com>
2024-03-03 10:04:05 +0530

    feat: added create file functionality

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
 
 fn main() {
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
+    .invoke_handler(tauri::generate_handler![greet, add_file, get_files])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
@@ -11,3 +11,37 @@ fn main() {
 fn greet(name: &str) -> String {
    format!("Hello, {}!", name)
 }
+// function to create new file
+#[tauri::command]
+fn add_file(fileName: &str, fileContent: &str, appDir:&str) ->String {
+  let filePath = std::path::Path::new(appDir).join(fileName);
+  // create the directory if it does not exist
+  if let Some(dir) = filePath.parent() {
+    std::fs::create_dir_all(dir).expect("Unable to create directory");
+  }
+  // save the file
+  if filePath.exists() {
+    return format!("File {} already exists", appDir.to_string() + fileName)
+  }
+  std::fs::write(&filePath, fileContent).expect("Unable to write file");
+  format!("File {} created successfully", appDir.to_string() + fileName)
+}
+
+// function to return all files and their content as object
+#[tauri::command]
+fn get_files(appDir:&str) -> serde_json::Value {
+  let mut files = serde_json::Map::new();
+  let path = std::path::Path::new(appDir);
+  if path.is_dir() {
+    for entry in std::fs::read_dir(path).expect("Unable to read directory") {
+      let entry = entry.expect("Unable to get entry");
+      let path = entry.path();
+      if path.is_file() {
+        let content = std::fs::read_to_string(&path).expect("Unable to read file");
+        files.insert(path.file_name().unwrap().to_string_lossy().to_string(), serde_json::Value::String(content));
+      }
+    }
+  }
+  serde_json::Value::Object(files)
+}
+ 
